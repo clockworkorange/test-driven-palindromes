f7f858e144142d8a095525302d2b08f5824570eb
Julio Ripoll Moreno <julio.ripoll@osoco.es>
2019-07-22 11:56:24 +0200

    palindromes ignores casing

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -4,4 +4,8 @@ describe('palindromes()', () => {
   it('correctly identifies one-word palindromes', () => {
     expect(palindromes('madam')).toEqual(['madam']);
   });
+
+  it('ignores casing', () => {
+    expect(palindromes('MaDaM')).toEqual(['madam']);
+  });
 });
